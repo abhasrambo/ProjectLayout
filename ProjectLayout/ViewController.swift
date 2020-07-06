aaa39182073cfd7f9d5a2406a80f638ec29abb72
Abhas Kumar <abhasroy@gmail.com>
2020-07-06 10:48:02 -0700

    Initial Commit

---
+++ ProjectLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Abhas Kumar on 7/6/20.
+//  Copyright © 2020 Abhas Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
